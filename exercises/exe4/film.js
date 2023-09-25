04b512c7b71ffc66ee990f12e7b8699fc8b45cf4
Aixufey <jack_11728@msn.com>
2023-09-25 20:19:48 +0200

    dynamically adding content with bootstrapping

--- exercises/exe4/film.js
+++ exercises/exe4/film.js
@@ -7,13 +7,14 @@ const btn = document.querySelector("#aBtn");
 
 
 btn.addEventListener('click', () => {
+    console.log('clicked')
     console.log(inputField.value)
     const found = FilmModule.getById(parseInt(inputField.value));
     if (found) {
-        article.innerHTML += `<h1>${found.name}</h1>` 
+        article.innerHTML += `<h1>${found.name}</h1>`
     } else {
         article.innerHTML += `<h1>Not found</h1>`
     }
-    
+
 })
 
